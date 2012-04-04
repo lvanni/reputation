30f49bd35dc11f1b8d1a5db7730042b597f6bbe6
rfritz <fritz.romain@etu.unice.fr>
2012-04-04 09:11:55 +0200

    adding lost in previous commit machine.java

---
+++ src/edu/lognet/reputation/model/user/Machine.java
@@ -0,0 +1,30 @@
+package edu.lognet.reputation.model.user;
+
+
+
+import edu.lognet.reputation.model.service.Service;
+
+public class Machine extends User {
+	
+	public static double IDEAL_BANDWIDTH = 1024;
+	public static double IDEAL_UPTIME = 10000000;
+
+	public Machine(String id, String name, int age, Service currentService,
+			providerType pType, raterType rType, collusionGroup cGroup,
+			double bandwidth, boolean is_available,int uptime, int resourceAvailable, int frequencyOfFluctuation) {
+		super(id, name, age, currentService, pType, rType, cGroup,0,
+				resourceAvailable, frequencyOfFluctuation);
+		double Qos = 0;
+		if((bandwidth/IDEAL_BANDWIDTH) > 1){
+			Qos += 0.4;
+		}
+		if((uptime/IDEAL_UPTIME)> 1){
+			Qos += 0.4;
+		}
+		if(is_available){
+			Qos +=0.2;
+		}
+		this.setQoS(Qos);
+	}
+
+}
